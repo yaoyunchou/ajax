be6a42331057170f45c0ea00a81bd48b09d687f6
yaoyunchou <303630573@qq.com>
2018-06-28 18:55:16 +0800

    完成ajax跨域的服务端操作

--- server/main.js
+++ server/main.js
@@ -1,6 +1,5 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
-const http = require('http');
 var Router = require('koa-router');
 const koalog4js = require('koa-log4');
 const app = new Koa();
@@ -10,32 +9,47 @@ const router = new Router();
 app.use(koaBody({
     multipart: true
 }));
+
+app.use( (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', '*');
+    ctx.set('Access-Control-Allow-Methods', '*');
+    next();
+});
 //TODO 引入http的监控  是否后面可以自己弄一个类似的,信息可以再完整一点
 app.use(koalog4js.koaLogger(koalog4js.getLogger('http'), {
     level: 'auto'
 }));
 
 
-
+/**
+ * 用于测试xhr的请求出现的情况
+ * 
+ */
 router.get('/ajaxserver/getjson', (ctx, next) => {
-    var callback = this.query["callback"];
-    if (!callback) return
-    this.type = 'text/javascript'
-    startChunk =  callback + '('
-    endChunk = ');'
-    //this.body =  startChunk + JSON.stringify(this.body) + endChunk;
     ctx.body = 'json';
 });
 
-
-app.on('error', (err, ctx) => {
-    console.error('server error', err, ctx)
+router.post('/ajaxserver/postJson', (ctx, next) => {
+    ctx.body = {
+        "data" : "postJson xiaofengqing"
+    };
 });
-app.use(router.routes());
 
+router.get('/ajaxserver/get1', (ctx, next) => {
+    let callbackName = ctx.query.callback || 'callback'
+    let backData =  `;${callbackName}(${JSON.stringify({"data" : "get1 ok"})})`
+    ctx.body = backData;
+
+    //
+});
+
+
+
+app.use(router.routes());
 app.use(router.allowedMethods());
 
 // export default app.listen(8070);
-module.exports = app.listen(8070, () => {
+app.listen(8070, () => {
     console.log('Koa is listening in 8070');
 });
